68def3e29b17e7788d8867c71f88303640b7a78f
ond9 <cyrill.zundler@heig-vd.ch>
2016-01-05 21:40:55 +0100

    Init project

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,12 +7,22 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
+router.get('/template/:name.html', function(req, res) {
+	console.log(req.params.name);
+	res.render('template/' + req.params.name);
+})
+
+router.get('/test', function (req, res, next) {
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      title: 'TWEB-ond9-Site1',
     });
-  });
 });
+
+// because of html 5 mode refresh certain page don't load index and fail 
+
+router.get('*', function(req, res){
+    res.render('index', {
+      title: 'TWEB-ond9-Site1',
+    });
+});
+
